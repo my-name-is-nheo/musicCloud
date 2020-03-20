91f7443af75e18373e5ee938c697b0a760a1cdcb
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-19 20:57:02 -0400

    progress bar done.

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,9 +1,9 @@
 var model = require("../model/index.js");
 const SongHandler = {
-  getRandomSong: (req, res, next) => {
+  getSong: (req, res, next) => {
     console.log(`here's a random queen song `);
     model.songs.getPlaylist(data => {
-      console.log(data);
+      // console.log(data);
       res.send(data);
       // console.log(JSON.stringify(data));
     });
