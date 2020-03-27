675c1819a717e19eed2aa8b3e0d4d5b5907bf2f0
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-27 18:18:02 -0400

    updated

--- client/src/styled_components/end_time.js
+++ client/src/styled_components/end_time.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 const Small_End_Time = styled.small`
   position: absolute;
   right: 0;
-  left: 880px;
-  top: 9px;
+  left: 874px;
+  top: 7px;
+  font: 100 14px/1.5 Interstate, Lucida Grande, Arial, sans-serif;
 `;
 
 export default Small_End_Time;
