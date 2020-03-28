b26ed9c41cacfca29702797a3a52e4968d94200b
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-28 14:37:57 -0400

    update 59

--- server/database/seed.sql
+++ server/database/seed.sql
@@ -204,28 +204,6 @@ Insert into songList (music_title, artist_name, album_cover, music_url) values (
 
 Insert into songList (music_title, artist_name, album_cover, music_url) values ("We Will Rock You", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/16+-+We+Will+Rock+You.mp3");
 
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("We Are The Champions", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/17+-+We+Are+The+Champions.mp3");
-
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Bohemian Rhapsody", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/01+-+Bohemian+Rhapsody.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Another One Bites The Dust", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/02+-+Another+One+Bites+The+Dust.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Killer Queen", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/03+-+Killer+Queen.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Fat Bottomed Girls", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/04+-+Fat+Bottomed+Girls.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Bicycle Race", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/05+-+Bicycle+Race.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("You're My Best Friend", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/06+-+You're+My+Best+Friend.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Don't Stop Me Now", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/07+-+Don't+Stop+Me+Now.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Save Me", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/08+-+Save+Me.mp3");
-
-Insert into songList (music_title, artist_name, album_cover, music_url) values ("Crazy Little Thing Called Love", "Queen","https://my-music-lists.s3.amazonaws.com/00-queen.jpg","https://my-music-lists.s3.amazonaws.com/09+-+Crazy+Little+Thing+Called+Love.mp3");
-
-
 
 
 
