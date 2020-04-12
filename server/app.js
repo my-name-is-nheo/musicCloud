ddef7407937670a042ec3655d2c9fbd0b481eabf
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-12 12:49:57 -0400

    heroku trial:

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT | 4000;
+const port = process.env.PORT || 4000;
 var cors = require("cors");
 const db = require("./database/index.js");
 const path = require("path");
