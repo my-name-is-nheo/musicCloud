77986ee380d257fd605736e9a361a2126fdb6104
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-12 17:39:35 -0400

    updated for heroku

--- server/controller/index.js
+++ server/controller/index.js
@@ -2,12 +2,8 @@ var model = require("../model/index.js");
 const SongHandler = {
   getSong: (req, res, next) => {
     console.log(`here's a random queen song `);
-    model.songs.getPlaylist((err, data) => {
-      if (err) {
-        throw err;
-        console.log(err, "in line 9 controller");
-      }
-      res.send(data);
+    model.songs.getPlaylist((data) => {
+      res.json(data);
     });
   },
 };
--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,8 @@ const mysql = require("mysql");
 
 // mysql://b3aa93e954fa25:ec1cb49a@us-cdbr-iron-east-01.cleardb.net/heroku_d0711f977976817?reconnect=true
 try {
-  var dbConnection = mysql.createConnection({
+  var dbPool = mysql.createPool({
+    connectionLimit: 10,
     host: "us-cdbr-iron-east-01.cleardb.net",
     user: "b3aa93e954fa25",
     password: "ec1cb49a",
@@ -12,10 +13,10 @@ try {
   console.log("mysql create connection error: ", e);
 }
 
-dbConnection.connect(function (err) {
-  if (err) {
-    console.log("failed to connect to db: ", err);
-  }
-});
+// dbPool.connect(function (err) {
+//   if (err) {
+//     console.log("failed to connect to db: ", err);
+//   }
+// });
 
-module.exports = dbConnection;
+module.exports = dbPool;
