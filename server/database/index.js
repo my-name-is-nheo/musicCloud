e17fe4be86444f521f4f15bf9cd170834e846b6d
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-12 14:12:25 -0400

    heroku update

--- server/database/index.js
+++ server/database/index.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 
 // mysql://b3aa93e954fa25:ec1cb49a@us-cdbr-iron-east-01.cleardb.net/heroku_d0711f977976817?reconnect=true
 var dbConnection = mysql.createConnection({
-  host:"us-cdbr-iron-east-01.cleardb.net",
+  host: "us-cdbr-iron-east-01.cleardb.net",
   user: "b3aa93e954fa25",
-  password:"ec1cb49a"
+  password: "ec1cb49a",
   database: "heroku_d0711f977976817",
 });
 
