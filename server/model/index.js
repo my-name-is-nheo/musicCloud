8a0ed428a022404e2a7ddee3bf2379a38ac449f0
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-12 16:01:43 -0400

    updated heroku

--- server/model/index.js
+++ server/model/index.js
@@ -2,9 +2,9 @@ var db = require("../database/index.js");
 module.exports = {
   songs: {
     getPlaylist: (callback) => {
-      var queryString = "select * from songList";
+      var queryString = "select * from songList;";
       // randomize the sqlChart.
-      return db.query(queryString, function (err, results) {
+      return db.query(queryString, (err, results) => {
         if (err) {
           console.log(err, " in line 9 of model/index.js");
           throw err;
