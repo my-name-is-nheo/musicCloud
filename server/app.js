5385fa307deebeda8c4ff35732da608d47b6c948
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-12 16:38:08 -0400

    updated

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.get("/getList", SongHandler.getSong);
 app.listen(port, function (err) {
   if (err) {
     throw err;
+    console.log(err);
   }
   console.log(`listening on port ${port}!`);
 });
