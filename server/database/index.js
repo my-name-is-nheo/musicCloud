f5107b5c619e90b8939755260fa8d61d3eca0708
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-12 14:28:14 -0400

    databse update

--- server/database/index.js
+++ server/database/index.js
@@ -1,12 +1,30 @@
 const mysql = require("mysql");
 
 // mysql://b3aa93e954fa25:ec1cb49a@us-cdbr-iron-east-01.cleardb.net/heroku_d0711f977976817?reconnect=true
-var dbConnection = mysql.createConnection({
+var db_config = {
   host: "us-cdbr-iron-east-01.cleardb.net",
   user: "b3aa93e954fa25",
   password: "ec1cb49a",
   database: "heroku_d0711f977976817",
-});
+};
+var connection;
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect(function (err) {
+    if (err) {
+      console.log("error connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
 
-dbConnection.connect();
+  connection.on("error", function (err) {
+    console.log(`db error`, err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+handleDisconnect();
 module.exports = dbConnection;
