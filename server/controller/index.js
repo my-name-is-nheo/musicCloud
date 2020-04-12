3c73ce9a87ed8f293bfcd2384184c76044179b41
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-12 16:42:50 -0400

    another update

--- server/controller/index.js
+++ server/controller/index.js
@@ -2,12 +2,15 @@ var model = require("../model/index.js");
 const SongHandler = {
   getSong: (req, res, next) => {
     console.log(`here's a random queen song `);
-    model.songs.getPlaylist(data => {
+    model.songs.getPlaylist((err, data) => {
       // console.log(data);
+      if (err) {
+        throw err;
+      }
       res.send(data);
       // console.log(JSON.stringify(data));
     });
-  }
+  },
 };
 
 module.exports = SongHandler;
